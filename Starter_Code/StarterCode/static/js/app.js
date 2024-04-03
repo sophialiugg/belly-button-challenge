89c0616e6c3a0207b3232f13056937bd4b4be984
sophialiugg <sliu9343@bths.edu>
2024-04-03 15:59:18 -0700

    commit

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,5 +1,5 @@
 //url variable
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
 //fetch JSON data and log it
 let data = d3.json(url).then(function(data) {
